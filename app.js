40f361361fdc1bf7387cef1aedf636fbb22b106c
iam_Vaibhav <vaibhavthobbi93@gmail.com>
2023-03-11 13:01:40 +0530

    workingwithfind_one_particularly

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const { getDb, connectToDb } = require("./db");
+const { ObjectId } = require("mongodb");
+
+// init app & middleware
 const app = express();
+
+// db connection
 let db;
-//we can communicate with database using db variable
+
 connectToDb((err) => {
   if (!err) {
     app.listen("3000", () => {
@@ -11,17 +16,34 @@ connectToDb((err) => {
     db = getDb();
   }
 });
-//routes
+
+// routes
 app.get("/books", (req, res) => {
   let books = [];
-  db.collection("books") //in shell ... db.books
-    .find() ////info in txt file
+
+  db.collection("books")
+    .find()
     .sort({ author: 1 })
-    .forEach((book) => books.push(book)) //async
+    .forEach((book) => books.push(book))
     .then(() => {
       res.status(200).json(books);
     })
     .catch(() => {
-      res.status(500).json({ error: "couldnt fetch" });
+      res.status(500).json({ error: "Could not fetch the documents" });
     });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could not fetch the document" });
+      });
+  } else {
+    res.status(500).json({ error: "Could not fetch the document" });
+  }
 });
